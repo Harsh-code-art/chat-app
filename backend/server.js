6641a9f9887974469bf748e9b13f8226f70950d5
Harsh <maxtrogamer854@gmail.com>
2026-02-12 17:51:39 +0530

    new change in backend

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirnames, "frontend", "dist", "index.html"));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 4000;
 
 server.listen(port, () => {
   console.log(` Server running on port ${port}`);
